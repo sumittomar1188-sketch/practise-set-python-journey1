a6f97ddf8602d26017d3f58f90e200db99eac2c9
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-01-25 20:28:36 +0530

    added some practise programs

---
+++ 02_problem.py
@@ -0,0 +1,10 @@
+# Install an external module and use it to perform an operation of your interest.  \
+# we installed pttsx3
+import pyttsx3
+engine = pyttsx3.init()
+
+    # For Mac, If you face error related to "pyobjc" when running the `init()` method :
+    # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+engine.say("I will speak this text")
+engine.runAndWait()
---
+++ 03_problem.py
@@ -0,0 +1,28 @@
+#  Write a python program to add two numbers. 
+s = 4
+t = 2
+print(s+t)
+
+# Write a python program to find remainder when a number is divided by z. 
+x = 24
+z = 5
+print(x%z)
+
+# Check the type of variable assigned using input () function. 
+num = input("enter a value , i wil give you its type :")
+print(type(num))
+
+'''Use comparison operator to find out whether ‘a’ given variable a is greater than 
+‘b’ or not. Take a = 34 and b = 80'''
+a = 34
+b = 80
+print(a>b)
+
+#  Write a python program to find an average of two numbers entered by the user. 
+n1 = int (input("enter a number :"))
+n2 = int (input("enter second number :"))
+print("average of the number :- ",(n1+n2)/2)
+
+
+s1 = int(input("enter a number :"))
+print("square of this number is",s1**2)
