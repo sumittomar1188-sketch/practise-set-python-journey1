2032040feb402d42d0967ee13176a3c161e9e99d
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-02-01 21:41:50 +0530

    added some practise questions

---
+++ 15_problem.py
@@ -0,0 +1,14 @@
+# find minimum number form given three number form user 
+a = int(input("enter 1st number :"))
+b = int(input("enter 2nd number :"))
+c = int(input("enter 3rd number :"))
+
+if a<b and a<c:
+    print(a)
+elif b<a and b<c :
+    print (b) 
+elif c<a and c<b:
+    print(c)
+
+else :
+    print("teeno barabar  he ")           
