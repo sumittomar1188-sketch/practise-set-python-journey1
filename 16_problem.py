e1f16519c6ed5c8dea73f1effd007fda3b0bd8c3
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-02-03 22:23:46 +0530

    added python practise question programs

---
+++ 16_problem.py
@@ -0,0 +1,24 @@
+# Print numbers from 1 to 10 using for loop
+
+# Print numbers from 10 to 1 using while loop
+
+# Print first 10 natural numbers
+
+# Print even numbers from 1 to 20
+
+# Print odd numbers from 1 to 20
+
+
+for i in range (1,11):
+    print(i)
+
+
+for i in range (10,0,-1):
+    print(i)    
+
+
+for i in range(2,21,2):
+    print(i)
+
+for i in range (1,21,2):
+    print(i)    
