e97eb52a88b5a0918e9b1f296881ac4aed927d0a
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-04-14 21:18:22 +0530

    python basic practise question

---
+++ 31_problem.py
@@ -0,0 +1,25 @@
+'''Problem 3: Login System (Mini Real Project)
+
+👉 Simple login system banao:
+
+🔹 Data:
+users = {
+  "admin": "1234",
+  "sumit": "pass"
+}
+🔹 Features:
+username input
+password input
+agar match → “Login Successful”
+warna → 3 attempts ke baad block
+🔹 Use:
+loop (attempts ke liye)
+dictionary (data ke liye)
+if-else (logic ke liye)'''
+
+
+s = 0 
+for i in range (1,4):
+    s = s+i
+
+print(s)    
---
+++ 32_problem.py
@@ -0,0 +1,9 @@
+import numpy as np 
+a = np.array([[10,20,30],
+              [40,50,60],
+              [70,80,90]])
+
+# (a) 50 print karo
+print(a[:1])
+# (b) 90 print karo
+# (c) 20 print karo
